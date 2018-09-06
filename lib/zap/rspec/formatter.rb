7b9fb28a2e8ae3971dad27aa4dc61d86b8784c6c
Ian Ker-Seymer <i.kerseymer@gmail.com>
2018-09-06 11:52:55 -0400

    Format exceptions

--- lib/zap/rspec/formatter.rb
+++ lib/zap/rspec/formatter.rb
@@ -7,20 +7,32 @@ module Zap
     # @private
     class Formatter < ::RSpec::Core::Formatters::BaseFormatter
       ::RSpec::Core::Formatters.register self,
-                                         :message,
+                                         :start,
+                                         :example_group_started,
                                          :example_started,
                                          :example_passed,
                                          :example_failed,
-                                         :example_pending
+                                         :example_pending,
+                                         :message,
+                                         :stop,
+                                         :start_dump,
+                                         :dump_pending,
+                                         :dump_summary,
+                                         :seed,
+                                         :close
+
+      def start(*args); end
+
+      def example_group_started(*args); end
 
       def example_started(notification)
         example = notification.example
         stream_example(example, event: :started, status: :running)
       end
 
-      def example_pending(notification)
+      def example_passed(notification)
         example = notification.example
-        stream_example(example, event: :completed, status: :skipped)
+        stream_example(example, event: :completed, status: :passed)
       end
 
       def example_failed(notification)
@@ -28,10 +40,24 @@ module Zap
         stream_example(example, event: :completed, status: :failed)
       end
 
-      def example_passed(notification)
+      def example_pending(notification)
         example = notification.example
-        stream_example(example, event: :completed, status: :passed)
+        stream_example(example, event: :completed, status: :skipped)
       end
+
+      def message(*args); end
+
+      def stop(*args); end
+
+      def start_dump(*args); end
+
+      def dump_pending(*args); end
+
+      def dump_summary(*args); end
+
+      def seed(*args); end
+
+      def close(*args); end
 
       private
 
@@ -51,20 +77,33 @@ module Zap
           event: event,
           status: status,
           time: now,
-          content: [
-            format_example_description(example)
-          ]
+          content: [*format_example_description(example),
+                    *format_example_exception(example)]
         }
       end
 
       def format_example_description(example)
-        {
+        [{
           message: example.full_description,
           source: {
             file: example.metadata[:file_path],
             start: example.metadata[:line_number]
           }
-        }
+        }]
+      end
+
+      def format_example_exception(example)
+        return [] unless example.exception
+
+        file, line, *_rest = example.exception.backtrace.first.split(':')
+
+        [{
+          message: example.exception.message,
+          source: {
+            file: file,
+            start: line.to_i
+          }
+        }]
       end
     end
   end
